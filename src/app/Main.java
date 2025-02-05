ba72d1472b28d98b04a4b4272ef1918793c24c63
Ed Gluschenko <edgluschenko3@gmail.com>
2025-02-05 23:27:50 +0200

    add miles to kilometers convert

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,7 +1,23 @@
 package app;
 
 public class Main {
-    public static void main(String[] args) {
+    private static final double CONV_L = 1.60934;
+    private static final double CONV_S = 1.852;
 
+    public static void main(String[] args) {
+        System.out.println("Converter app.");
+        System.out.println("Converter mile to kilometers");
+        double mile = 5;
+
+        double kilometer = convMileLandToKilometer(mile);
+        double kilometer1 = convMileSeaToKilometer(mile);
+        System.out.println("Result is " + kilometer + " kilometers, if land mile " + kilometer1 + " kilometers, if sea mile ");
+
+    }
+    private static double convMileLandToKilometer(double mile){
+        return mile * CONV_L;
+    }
+    private static double convMileSeaToKilometer(double mile){
+        return mile * CONV_S;
     }
 }
